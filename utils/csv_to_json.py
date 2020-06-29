1c6895146ac2ca6f8adacdd9c73649cdb7a8841e
Vaernion <vaporz@gmail.com>
2020-06-30 01:26:05 +0200

    helpful error handling, flexible arguments

--- utils/csv_to_json.py
+++ utils/csv_to_json.py
@@ -1,44 +1,47 @@
 import argparse
 import json
-import os
 import sys
 
 
 def arguments():
     parser = argparse.ArgumentParser(
-        description="Custom CSV to JSON converter for internal use",
+        description="CSV to JSON converter",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    parser.add_argument("-i", "--input", help="input filename", type=str, required=True)
     parser.add_argument(
-        "-o", "--output", help="output filename", type=str, required=True
+        "-i", "--input", help="File to read CSV from", type=str, required=True,
     )
     parser.add_argument(
-        "-s",
-        "--separator",
-        help="field separator",
+        "-o",
+        "--output",
+        help="File to write JSON to (will overwrite existing)",
+        required=True,
         type=str,
-        default=";",
-        choices=[",", ";"],
+    )
+    parser.add_argument(
+        "-s", "--separator", help="Field separator", type=str, default=";",
+    )
+    parser.add_argument(
+        "-f,",
+        "--fields",
+        help="Comma separated headers, will use first line in file if not supplied",
+        type=str,
     )
     return parser.parse_args()
 
 
 def readFile(path):
-    if os.path.isfile(path):
-        try:
-            with open(path, "r", encoding="utf-8") as f:
-                string = f.read()
-        except IOError as e:
-            print(e)
-        else:
-            return string
-    else:
-        print(f"{path} not found")
+    try:
+        with open(path, "r", encoding="utf-8") as f:
+            string = f.read()
+    except IOError as e:
+        print(f"IOError: {e}")
         sys.exit(1)
+    else:
+        return string
 
 
-def csvParser(csv, separator):
+def csvParser(csv, args):
     data = []
     lines = csv.splitlines()
 
@@ -46,37 +49,44 @@ def csvParser(csv, separator):
     if lines[0][0] == "\ufeff":
         lines[0] = lines[0].replace("\ufeff", "")
 
-    headers = lines[0].split(separator)
+    headers = args.fields.split(",") if args.fields else lines[0].split(args.separator)
     print("headers", headers)
 
-    for i, line in enumerate(lines):
-        if i == 0:
-            continue
-        fields = line.split(separator)
-        record = {}
-        for j, header in enumerate(headers):
-            record[header] = fields[j]
-        data.append(record)
-    return data
+    try:
+        for i, line in enumerate(lines):
+            if i == 0 and not args.fields:  # don't include headers
+                continue
+            fields = line.split(args.separator)
+            record = {}
+            for j, header in enumerate(headers):
+                record[header] = fields[j]
+            data.append(record)
+    except IndexError as e:
+        print(f"IndexError: {e}\nCSV file has missing fields on line {i+1}")
+        sys.exit(1)
+    else:
+        return data
 
 
-def writeJson(outPath, data):
+def writeJson(data, outPath):
     try:
         jsonData = json.dumps(data, indent=2, ensure_ascii=False)
         with open(outPath, "w", encoding="utf-8") as f:
             f.write(jsonData)
             print(f"{len(data)} records saved to {outPath} length {len(jsonData)}")
     except IOError as e:
-        print(e)
+        print("IOError: {e}")
+        sys.exit(1)
 
 
 def main():
     args = arguments()
-    print("input: ", args.input, "\noutput: ", args.output)
+    print("separator:", args.separator)
+    print(f"input: {args.input}\noutput: {args.output}")
 
     csv = readFile(args.input)
-    data = csvParser(csv, args.separator)
-    writeJson(args.output, data)
+    data = csvParser(csv, args)
+    writeJson(data, args.output)
 
 
 if __name__ == "__main__":
