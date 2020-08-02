d452a11e7b01baf2114abfb771d14dddd6be719d
vaernion <55745090+vaernion@users.noreply.github.com>
2020-08-02 01:22:41 +0000

    Deploying to gh-pages from  @ 1828538494995ed30a70afd3fdba13116b186525 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/media-sheet-viewer/precache-manifest.b086097652a6447cc9b9ab076600cd24.js"
+  "/media-sheet-viewer/precache-manifest.0eb63e8d48632ed7293d9c1a40158ba1.js"
 );
 
 self.addEventListener('message', (event) => {
